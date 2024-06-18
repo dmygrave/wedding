d5972a6ce85eb17377c74bf940f64d92d5d1a3ff
Dmygrave <needdspeedd@gmail.com>
2024-06-18 20:37:45 +0300

    немного по мелочи

--- src/components/WeddingPage.vue
+++ src/components/WeddingPage.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="container">
     <div class="newlyweds">
-
+      <div class="names-container">
+        <div class="sub">Женитьба</div>
+        <div class="names">Дмитрий и Анна</div>
+      </div>
+      <div class="photo"></div>
+      <div class="text">
+        Дорогие родные и близкие!<br>
+        Мы приглашаем вас разделить с нами важный день нашей жизни — нашу свадьбу.
+      </div>
     </div>
   </div>
 </template>
@@ -14,6 +22,37 @@ export default {
 
 
 <style scoped>
+
+.newlyweds {
+  margin: 30px 0;
+}
+
+.names-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.names {
+  font-size: 38px;
+  line-height: 0.8;
+  font-weight: 700;
+  margin-top: 15px;
+  font-family: 'Great Vibes', cursive;
+}
+
+.photo {
+  border-radius: 15px;
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url('@/assets/images/love.jpg');
+  height: 430px;
+  width: 100%;
+  margin: 20px auto;
+}
 
 </style>
 
