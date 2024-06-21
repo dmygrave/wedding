bc921c611e7f25ceb894666df8eaa6504bf58b66
Dmygrave <needdspeedd@gmail.com>
2024-06-21 22:48:44 +0300

    Updates

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap';
-import './assets/main.scss';
-
-createApp(App).mount('#app')
